ed144054c199f3882016efc51e745ee44aefe8e3
wangzi <wangzi>
2017-08-05 13:52:57 +0800

    review

--- springboot-tbschedule/src/main/java/com/wz/TbScheduleApplication.java
+++ springboot-tbschedule/src/main/java/com/wz/TbScheduleApplication.java
@@ -10,5 +10,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TbScheduleApplication {
     public static void main(String[] args) {
         SpringApplication.run(TbScheduleApplication.class, args);
+        synchronized (TbScheduleApplication.class){
+            while (true){
+                try{
+                    TbScheduleApplication.class.wait();
+                }catch (Throwable throwable){
+                }
+            }
+        }
     }
 }
