e7e62993988eff56dbef7c26436eb7b8708a16a7
wangzi <wangzi>
2017-08-05 13:12:11 +0800

    review

--- springboot-dubbo-consumer/src/main/java/com/wz/ConsumerApplication.java
+++ springboot-dubbo-consumer/src/main/java/com/wz/ConsumerApplication.java
@@ -1,12 +1,26 @@
 package com.wz;
 
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.wz.api.UserService;
+import com.wz.service.UserConsumerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class ConsumerApplication {
 
+    @Autowired
+    private UserConsumerService userService;
+
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class, args);
+    }
+
+    @PostConstruct
+    public void hello(){
+        System.out.println(userService.hello());
     }
 }
