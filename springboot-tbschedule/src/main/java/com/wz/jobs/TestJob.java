82b8833d11cf65b3c54aa030de5f8f2c05bd96c3
wangzi <wangzi>
2017-08-05 16:45:08 +0800

    review

--- springboot-tbschedule/src/main/java/com/wz/jobs/TestJob.java
+++ springboot-tbschedule/src/main/java/com/wz/jobs/TestJob.java
@@ -6,6 +6,7 @@ import com.wz.config.JedisClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
@@ -21,16 +22,16 @@ public class TestJob implements IScheduleTaskDealMulti<String> {
     @Override
     public List<String> selectTasks(String s, String s1, int i, List<TaskItemDefine> list, int i1) throws Exception {
         System.out.println("开始获取任务...");
-        List<String> tasks = null;
+        List<String> tasks = new ArrayList<>();
         // 在获取不到任务时，执行将停止
         // 此处用Arrays.asList("a", "b", "c")来模拟从数据库取数据的情况
         // 同时在execute中将运行成功的任务放入redis
         for (String id : Arrays.asList("a", "b", "c")){
-            if (jedis.get(id) == null){
+            if (jedis.get(SUFFIX_JOB + id) == null){
                 tasks.add(id);
             }
         }
-        return tasks;
+        return tasks.size()>0? tasks : null;
     }
 
     @Override
